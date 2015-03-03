9d52b43a516568b1e161329323374c9fd40427af
djjmz <djjmz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-03 11:25:13 +0000

    Bug fix
    git-svn-id: https://plugins.svn.wordpress.org/simple-auto-tag/trunk@1104047 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simpleautotag.php
+++ simpleautotag.php
@@ -66,7 +66,13 @@
 		Words (one word per line):<br>
 		<textarea rows="4" cols="50" name="words">'.$file_read.'</textarea><br>
 		<input type="submit" value="Submit">
-		</form>
+		</form><br><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="5NTQ9DTFUDX5L">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
 		</div>
 		</div>
 		</div>
